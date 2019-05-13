d273e3d526064fe371847372d4f646572e7e515a
Prosper Ibhamawu <Prosper.Ibhamawu@Agrium.com>
2019-05-13 11:46:55 -0600

    started from strach

--- resource.tf
+++ resource.tf
@@ -25,17 +25,3 @@ resource "aws_instance" "test" {
 resource "aws_security_group" "instance" {
     vpc_id = "vpc-013f49bf900c4e52d"
 }
-
-resource "aws_autoscaling_group" "test" {
-    //this is to extract values from the the created
-    launch_configuration = "${aws_instance.test.ami}"
-
-    min_size = "${var.min_size}"
-
-    max_size = "${var.max_size}"
-
-}
-
-output "asg_name" {
-  value = "${aws_autoscaling_group.test.name}"
-}
--- vars.tf
+++
@@ -1,7 +0,0 @@
-variable "min_size" {
-  description = "The minimum amount of EC2 instance"
-}
-
-variable "max_size" {
-  description = "The maximum number of EC2 Instances in the ASG"
-}
